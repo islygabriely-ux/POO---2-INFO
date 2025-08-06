da280a8f6da6284002190f44283e5c22396b1338
islygabriely-ux <isly.gabriely@escolar.ifrn.edu.br>
2025-08-06 13:59:13 -0300

    Polimorfismo.java

--- codigo-polimorfismo/Polimorfismo.java
+++ codigo-polimorfismo/Polimorfismo.java
@@ -1,9 +1,32 @@
-class Instrumento {
-  public void tocar() {
-    System.out.printl("O instrumento está tocando");
-  }
+ class Instrumento {
+    public void tocar() {
+        System.out.println("O instrumento está tocando");
+    }
 }
-
 class Violao extends Instrumento {
-  public void tocar 
+    public void tocar() {
+        System.out.println("Tocando violão...");
+    }
+}
+class Bateria extends Instrumento {
+    public void tocar() {
+        System.out.println("Tocando bateria...");
+    }
+}
+class Flauta extends Instrumento {
+    public void tocar() {
+        System.out.println("Tocando flauta...");
+    }
+}
+public class Concerto {
+    public static void main(String[] args) {
+        Instrumento i1 = new Violao();
+        Instrumento i2 = new Bateria();
+        Instrumento i3 = new Flauta();
+
+        i1.tocar();
+        i2.tocar();
+        i3.tocar();
+    }
+}
   
